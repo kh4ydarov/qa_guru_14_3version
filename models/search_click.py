67f7d2e3a021f635385c616a0bb82acf71cb2153
kh4ydarov <kh4ydarovs@yandex.ru>
2024-05-19 10:44:21 +0500

    hw

--- models/search_click.py
+++ models/search_click.py
@@ -9,7 +9,7 @@ class Search:
         with allure.step("Поиск определленого фильма и переход в карточку фильма"):
             browser.element('a.Navbar__link[aria-label="Поиск"]').click()
             browser.element('.search-bar').should(be.visible).should(be.clickable).click()
-            browser.element('.input').set_value('Терминатор')
+            browser.element('.input').set_value('Терминатор').press_enter()
             browser.element('.marquee-text__scroll').perform(command.js.scroll_into_view).click()
             browser.element('.marquee-text__scroll').should(have.text('Терминатор'))
         return self
